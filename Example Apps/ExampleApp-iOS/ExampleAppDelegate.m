6ef348532588ff60c4ae4357d5fe26c0465aab9a
wbitos <wbitos@126.com>
2022-01-06 15:21:02 +0800

    Remove UIWebView

--- Example Apps/ExampleApp-iOS/ExampleAppDelegate.m
+++ Example Apps/ExampleApp-iOS/ExampleAppDelegate.m
@@ -1,5 +1,4 @@
 #import "ExampleAppDelegate.h"
-#import "ExampleUIWebViewController.h"
 #import "ExampleWKWebViewController.h"
 
 @implementation ExampleAppDelegate
@@ -7,12 +6,12 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     // 1. Create the UIWebView example
-    ExampleUIWebViewController* UIWebViewExampleController = [[ExampleUIWebViewController alloc] init];
-    UIWebViewExampleController.tabBarItem.title             = @"UIWebView";
+//    ExampleUIWebViewController* UIWebViewExampleController = [[ExampleUIWebViewController alloc] init];
+//    UIWebViewExampleController.tabBarItem.title             = @"UIWebView";
 
     // 2. Create the tab footer and add the UIWebView example
     UITabBarController *tabBarController = [[UITabBarController alloc] init];
-    [tabBarController addChildViewController:UIWebViewExampleController];
+//    [tabBarController addChildViewController:UIWebViewExampleController];
     
     // 3. Create the  WKWebView example for devices >= iOS 8
     if([WKWebView class]) {
